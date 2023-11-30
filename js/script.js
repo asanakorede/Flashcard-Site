6f78e5d8948009ba85f5257ad212cd7a057d6350
Asana Korede <asanakorededaniel@gmail.com>
2023-11-30 14:58:05 -0800

    add definitions for the first five buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    $(".clickable").click(function () {
+        $("#Javascript").fadeIn().toggle();
+    });
+
+    $(".clickable").click(function () {
+        $("#Operators").fadeIn().toggle();
+    });
+
+    $(".clickable").click(function () {
+        $("#Variables").fadeIn().toggle();
+    });
+
+    $(".clickable").click(function () {
+        $("#Functions").fadeIn().toggle();
+    });
+
+    $(".clickable").click(function () {
+        $("#Methods").fadeIn().toggle();
+    });
 })
